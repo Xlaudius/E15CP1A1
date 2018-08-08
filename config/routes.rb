21f57c8eeeaa788f9025b3514e5cea77c28ac4a6
Claudio Rivera <claudio.rivera.sh@gmail.com>
2018-08-07 21:26:24 -0400

    ADD: users helpers, sessions, better errors gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   resources :histories
+  resources :sessions, only: [:create, :destroy]
 
   get 'users/sign_up', to: "users#new"
   post 'users', to: "users#create"
+
 
   root "histories#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
